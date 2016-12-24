f23a2291ada412ed2f19dab8dcd3741d0b4aa23f
harryhan24 <herryhan2435@gmail.com>
2016-12-24 15:53:55 +0900

    ok

--- server/server.js
+++ server/server.js
@@ -1,16 +1,31 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 
 const port = 3000;
 var app = express();
+//이 방식이나 app 방식이나 차이가 없다
+var server = http.createServer(app);
+
+//알아서 io를 열어줌
+var io = socketIO(server);
 
 //use middleware
 app.use(express.static(publicPath));
 
-app.listen(3000, ()=>{
-   console.log(`server is running ${port}`);
+io.on('connection', (socket)=>{
+   console.log('New User Connected');
 
+   //connection으로 메세지 보내기
+   socket.on('disconnect', ()=>{
+      console.log('User was disconnted');
+
+   });
+
+});
+server.listen(3000, ()=>{
+   console.log(`server is running ${port}`);
 });
